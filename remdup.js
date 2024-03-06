d3535f1e79ee1c96f1880be691634dccb797b568
santhosh0023 <santhosh020101@gmail.com>
2024-03-06 14:06:39 +0530

    add html files

--- remdup.js
+++ remdup.js
@@ -6,4 +6,5 @@ const inputArray = [1, 2, 3, 4, 5, 1, 6, 2, 7, 1, 3];
 const uniqueArray = removeDuplicates(inputArray);
 console.log("The output is :",uniqueArray);
 
-    
+document.getElementById("uniq").innerHTML = `
+The output is : ${uniqueArray}`
